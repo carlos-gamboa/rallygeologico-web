e135104073c3933260eb0d866bf31033e3a8664a
Ana Laura Vargas <alauvargas@gmail.com>
2018-05-07 16:36:17 -0600

    Conectarse con API de google maps

--- rallygeologico-app/src/app/app.module.ts
+++ rallygeologico-app/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from "./app-routing.module";
-
-import { RegisterComponent } from './register/register.component';
-import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from "./login/login.component";
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {RegisterComponent} from './register/register.component';
+import {LandingComponent} from './landing/landing.component';
+import {LoginComponent} from "./login/login.component";
 import {CommonModule} from "@angular/common";
 import {UserModule} from "./user/user.module";
 import {AboutUsComponent} from "./about-us/about-us.component";
@@ -19,7 +18,7 @@ import {RalliesComponent} from "./rallies/rallies.component";
 import {RallyComponent} from "./rally/rally.component";
 import {PublicHeaderComponent} from "./public-header/public-header.component";
 import {PublicFooterComponent} from "./public-footer/public-footer.component";
-import { FacebookModule } from 'ngx-facebook';
+import {FacebookModule} from 'ngx-facebook';
 import {DataService} from "./services/data/data.service";
 import {UserService} from "./services/user.service";
 import {Configuration} from "./services/data/constants";
@@ -27,6 +26,8 @@ import {HttpClient, HttpClientModule, HttpHandler} from "@angular/common/http";
 import {RallyService} from "./services/rally.service";
 import {InvitationService} from "./services/invitation.service";
 import {CompetitionService} from "./services/competition.service";
+import {AgmCoreModule} from '@agm/core';
+import {AgmSnazzyInfoWindowModule} from '@agm/snazzy-info-window';
 
 
 @NgModule({
@@ -54,7 +55,11 @@ import {CompetitionService} from "./services/competition.service";
       NgbModule,
       UserModule,
       FacebookModule.forRoot(),
-      HttpClientModule
+      HttpClientModule,
+      AgmCoreModule.forRoot({
+          apiKey: 'AIzaSyB2x45eM22K3ILg7W2jYnEAJ4tGYmODqjg'
+      }),
+      AgmSnazzyInfoWindowModule
   ],
   providers: [DataService, UserService, Configuration, RallyService, InvitationService, CompetitionService],
   bootstrap: [AppComponent]
