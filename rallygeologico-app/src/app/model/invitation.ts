4d9b736ac8e97ffcc87f88276b1e625024e571f8
carlos-gamboa <cali1997gv@hotmail.com>
2018-05-07 20:58:36 -0600

    Dashboard | Display invitations

--- rallygeologico-app/src/app/model/invitation.ts
+++ rallygeologico-app/src/app/model/invitation.ts
@@ -1,9 +1,14 @@
+import {User} from "./user";
+import {Competition} from "./competition";
+
 export class Invitation {
     id : number;
     accepted : boolean;
     user_id_send : number;
     user_id_receive : number;
     competition_id : number;
+    user_send: User;
+    competition: Competition;
 
     constructor(invitation: any){
         this.id = invitation.id;
@@ -11,5 +16,7 @@ export class Invitation {
         this.user_id_send = invitation.user_id_send;
         this.user_id_receive = invitation.user_id_receive;
         this.competition_id = invitation.competition_id;
+        this.user_send = invitation.user_send;
+        this.competition = invitation.competition;
     }
 }
