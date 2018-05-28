67547febdcb235d09c6147c0c7d10ca970079f22
JhosuaArias <jhosuarqui@hotmail.com>
2018-05-28 16:44:23 -0600

    Profile BE and FE progress

--- rallygeologico-app/src/app/user/profile/profile.component.ts
+++ rallygeologico-app/src/app/user/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {User} from "../../model/user";
+import {DataService} from "../../services/data/data.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user : User;
+  constructor(private dataService: DataService,
+              private userService: UserService,
+              private router: Router) {
+      this.user = this.dataService.getUser();
+      if (!this.user) {
+          this.userService.isLoggedIn().subscribe((users: User) => {
+              if (users[0]) {
+                  this.dataService.updateUser(users[0]);
+                  this.user = users[0];
+                  this.setupData();
+              } else {
+                  this.router.navigate(['/landing']);
+              }
+          });
+      } else {
+          this.setupData();
+      }
+  }
 
   ngOnInit() {
   }
 
+  setupData(){
+
+  }
 }
