4e989d5f0f3d7270b5e2d22e43329f4743326eff
carlos-gamboa <cali1997gv@hotmail.com>
2018-05-31 16:42:33 -0600

    Edit competition

--- rallygeologico-app/src/app/admin/admin.module.ts
+++ rallygeologico-app/src/app/admin/admin.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { CompetitionComponent } from './competition/competition.component';
 import { EditCompetitionComponent } from './edit-competition/edit-competition.component';
 import {PublicFooterComponent} from "./public-footer/public-footer.component";
 import {AdminRoutingModule} from "./admin-routing.module";
@@ -13,7 +12,6 @@ import {AdminComponent} from "./admin.component";
       AdminRoutingModule
   ],
   declarations: [LoginComponent,
-      CompetitionComponent,
       EditCompetitionComponent,
       PublicFooterComponent,
       AdminComponent],
