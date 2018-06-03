18a61ec9e8e28796c68c1b4f798a2e91014cff10
carlos-gamboa <cali1997gv@hotmail.com>
2018-06-03 10:07:36 -0600

    Use a table to select the desired competition to edit

--- rallygeologico-app/src/app/admin/edit-competition/edit-competition.component.ts
+++ rallygeologico-app/src/app/admin/edit-competition/edit-competition.component.ts
@@ -20,7 +20,10 @@ import {DataService} from "../../services/data/data.service";
 export class EditCompetitionComponent implements OnInit {
 
     ralliesList: Rally[] = [];
-    competitionList: Competition[] = [];
+
+    competitions: Competition[];
+    allCompetitions: Competition[];
+    showedCompetitions: Competition[];
 
     user: User;
     users : User[];
@@ -28,8 +31,10 @@ export class EditCompetitionComponent implements OnInit {
     showedUsers: User[];
 
     pageSize : number = 10;
-    currentPage : number = 0;
+    currentPageUser : number = 0;
     totalUsers : number = 0;
+    totalCompetitions : number = 0;
+    currentPageCompetition: number = 0;
 
     searchQuery : string = "";
 
@@ -47,6 +52,9 @@ export class EditCompetitionComponent implements OnInit {
 
     newCompetition: boolean;
     changesSaved: boolean;
+    competitionSelected: boolean;
+    readyToShow: boolean;
+    activeTab: number;
 
     invitedUsers: number[] = [];
 
@@ -55,8 +63,7 @@ export class EditCompetitionComponent implements OnInit {
                 private invitationService: InvitationService,
                 private competitionStatisticsService: CompetitionStatisticsService,  private router: Router,
                 private dataPipe: DatePipe) {
-        this.changesSaved = false;
-        this.currentCompetitionIndex = -1;
+        this.readyToShow = false;
         this.user = this.dataService.getUser();
         if (!this.user) {
             this.userService.isLoggedIn().subscribe((users: User) => {
@@ -81,24 +88,27 @@ export class EditCompetitionComponent implements OnInit {
             for (let i: number = 0; i < rallies.length; ++i) {
                 this.ralliesList.push(rallies[i]);
             }
-        });
-        this.competitionService.getAllCompetitions().subscribe((competitions: Competition[]) => {
-            for (let i: number = 0; i < competitions.length; ++i) {
-                this.competitionList.push(competitions[i]);
-            }
-            this.currentCompetitionIndex = 0;
-            this.editCompetitionChange();
-        });
-        this.allUsers = [];
-        this.userService.getUsers().subscribe((users: User[]) => {
-            for (let user of users){
-                if (user.id != this.user.id){
-                    this.allUsers.push(user);
+            this.allCompetitions = [];
+            this.competitionService.getAllCompetitions().subscribe((competitions: Competition[]) => {
+                for (let i: number = 0; i < competitions.length; ++i) {
+                    this.allCompetitions.push(competitions[i]);
                 }
-            }
-            this.reloadUsers(this.allUsers);
-            //console.log(this.allUsers);
+                this.reloadCompetitions(this.allCompetitions);
+                this.allUsers = [];
+                this.userService.getUsers().subscribe((users: User[]) => {
+                    for (let user of users){
+                        if (user.id != this.user.id){
+                            this.allUsers.push(user);
+                        }
+                    }
+                    this.reloadUsers(this.allUsers);
+                    this.competitionSelected = false;
+                    this.readyToShow = true;
+                });
+            });
         });
+
+
     }
 
     /**
@@ -109,15 +119,15 @@ export class EditCompetitionComponent implements OnInit {
         this.users = users;
         this.totalUsers = users.length;
         this.showedUsers = users.slice(0, this.pageSize);
-        this.currentPage = 0;
+        this.currentPageUser = 0;
     }
 
     /**
      * Selects the number of users' pages
      */
-    pageChange() : void{
+    userPageChange() : void{
         if(this.users) {
-            this.showedUsers = this.users.slice((this.currentPage - 1) * this.pageSize, ((this.currentPage) * this.pageSize));
+            this.showedUsers = this.users.slice((this.currentPageUser - 1) * this.pageSize, ((this.currentPageUser) * this.pageSize));
         }
     }
 
@@ -125,6 +135,43 @@ export class EditCompetitionComponent implements OnInit {
      * Searches a specified user
      */
     searchUser(){
+        let usersToShow = [];
+        if(this.searchQuery.length >= 1) {
+            for (let user of this.allUsers) {
+                if (user.username.toLowerCase().startsWith(this.searchQuery.toLowerCase())) {
+                    usersToShow.push(user);
+                }
+            }
+            this.reloadUsers(usersToShow);
+        }else{
+            this.reloadUsers(this.allUsers);
+        }
+    }
+
+    /**
+     * Reloads the corresponding users in the table
+     * @param {User[]} users
+     */
+    reloadCompetitions(competitions : Competition[]) : void{
+        this.competitions = competitions;
+        this.totalCompetitions = competitions.length;
+        this.showedCompetitions = competitions.slice(0, this.pageSize);
+        this.currentPageCompetition = 0;
+    }
+
+    /**
+     * Selects the number of users' pages
+     */
+    competitionPageChange() : void{
+        if(this.users) {
+            this.showedUsers = this.users.slice((this.currentPageCompetition - 1) * this.pageSize, ((this.currentPageCompetition) * this.pageSize));
+        }
+    }
+
+    /**
+     * Searches a specified user
+     */
+    searchCompetition(){
         let usersToShow = [];
         if(this.searchQuery.length >= 1) {
             for (let user of this.allUsers) {
@@ -152,8 +199,7 @@ export class EditCompetitionComponent implements OnInit {
     }
 
     editCompetitionChange(){
-        if (this.currentCompetitionIndex == 0){
-            this.currentCompetition = new Competition(null);
+        if (!this.currentCompetition){
             this.name = "";
             this.is_active = "1";
             this.is_public = "1";
@@ -162,7 +208,6 @@ export class EditCompetitionComponent implements OnInit {
             this.starting_date = "";
             this.finishing_date = "";
         } else {
-            this.currentCompetition = this.competitionList[this.currentCompetitionIndex - 1];
             this.name = this.currentCompetition.name;
             this.is_active = this.currentCompetition.is_active.toString();
             this.is_public = this.currentCompetition.is_public.toString();
@@ -171,6 +216,21 @@ export class EditCompetitionComponent implements OnInit {
             this.starting_date = this.dataPipe.transform(this.currentCompetition.starting_date, 'yyyy-MM-ddThh:mm');
             this.finishing_date = this.dataPipe.transform(this.currentCompetition.finishing_date, 'yyyy-MM-ddThh:mm');
         }
+    }
+
+    edit(i: number){
+        this.activeTab = 0;
+        this.competitionSelected = true;
+        if (i == -1){
+            this.currentCompetition = null;
+        } else {
+            this.currentCompetition = this.showedCompetitions[i];
+        }
+        this.editCompetitionChange();
+    }
+
+    changeTab(i: number){
+        this.activeTab = i;
     }
 
 }
